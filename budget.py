b17564bbd25c1c9fb99393dbb0a30398f4c268e3
t0varish <52796958+t0vraish@users.noreply.github.com>
2021-07-26 02:22:12 +0200

    Added some methods to the class

--- budget.py
+++ budget.py
@@ -1,7 +1,29 @@
 class Category:
+    ledger = list()
+    fund = 0
 
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
+        self.fund += amount
 
+    def withdraw(self, amount, description=""):
+        if self.fund < amount:
+            return False
+        else:
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
 
+    def get_balance(self):
+        print(self.fund)
+
+    def transfer(amount, dest_categ):
+        pass
+
+    def check_funds():
+        pass
+
+    pass
 
 
 def create_spend_chart(categories):
+    pass
